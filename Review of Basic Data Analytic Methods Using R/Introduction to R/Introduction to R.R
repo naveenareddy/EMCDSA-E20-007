c6e33ccf8a70f54cf71f4383e3f5c8c3e65a9adb
sumendar karupakala <sumendar@gmail.com>
2015-08-05 19:06:25 +0530

    change
    change

--- Review of Basic Data Analytic Methods Using R/Introduction to R/Introduction to R.R
+++
@@ -1,5 +0,0 @@
-setwd("C:/Users/Sri/Documents/GitHub/RWD/EMCDSA-E20-007/Review of Basic Data Analytic Methods Using R/Introduction to R")
-sales <- read.csv("C:/Users/Sri/Documents/GitHub/RWD/EMCDSA-E20-007/Review of Basic Data Analytic Methods Using R/Introduction to R/yearly_sales.csv")
-head(sales)
-##########R Graphical User Interfaces############
-
