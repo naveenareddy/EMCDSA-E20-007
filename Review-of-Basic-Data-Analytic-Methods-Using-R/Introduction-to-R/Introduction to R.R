2afc63b020d6f311983f1f44891332a8df1df590
sumendar karupakala <sumendar@gmail.com>
2015-08-05 19:27:26 +0530

    introduction to R
    introduction to R

--- Review-of-Basic-Data-Analytic-Methods-Using-R/Introduction-to-R/Introduction to R.R
+++ Review-of-Basic-Data-Analytic-Methods-Using-R/Introduction-to-R/Introduction to R.R
@@ -1,1 +1,8 @@
 
+setwd("C:/Users/Sri/Documents/GitHub/RWD/EMCDSA-E20-007/Review-of-Basic-Data-Analytic-Methods-Using-R/Introduction-to-R")
+########Introduction to R##############
+#import a CSV file of the totatl annaual sales for each customer
+sales <- read.csv("yearly_sales.csv") 
+#examin the imported data set
+head(sales) 
+#########R Graphical User Interfaces##############
