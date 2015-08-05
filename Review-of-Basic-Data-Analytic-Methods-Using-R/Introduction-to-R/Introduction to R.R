833b921786b215e7f7ec45f17acc3fd8f990d006
sumendar karupakala <sumendar@gmail.com>
2015-08-05 19:50:55 +0530

    intro to R

--- Review-of-Basic-Data-Analytic-Methods-Using-R/Introduction-to-R/Introduction to R.R
+++ Review-of-Basic-Data-Analytic-Methods-Using-R/Introduction-to-R/Introduction to R.R
@@ -5,4 +5,16 @@ setwd("C:/Users/Sri/Documents/GitHub/RWD/EMCDSA-E20-007/Review-of-Basic-Data-Ana
 sales <- read.csv("yearly_sales.csv") 
 #examin the imported data set
 head(sales) 
+summary(sales)
+str(sales)
+#plot number of orders vs sales
+plot(sales$num_of_orders,sales$sales_total,main = "Number of Orders vs Sales")
+#perform statistical analysis (fit a linear regression model)
+results <- lm(sales$sales_total ~ sales$num_of_orders)
+results
+summary(results)
+attributes(results)
+#perform some diagnostics on the fittered model
+#plot histogram of the residuals
+hist(results$residuals,breaks = 800)
 #########R Graphical User Interfaces##############
