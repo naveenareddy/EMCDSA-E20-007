864a938863bc60939418161b66b52cdffb78eba3
sumendar karupakala <sumendar@gmail.com>
2015-08-05 18:54:04 +0530

    Introduction to R
    Introduction to R

---
+++ Review of Basic Data Analytic Methods Using R/Introduction to R.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/Sri/Documents/GitHub/RWD/EMCDSA-E20-007/Review of Basic Data Analytic Methods Using R")
+sales <- read.csv("C:/Users/Sri/Documents/GitHub/RWD/EMCDSA-E20-007/Review of Basic Data Analytic Methods Using R/yearly_sales.csv")
+head(sales)
+##########R Graphical User Interfaces############
+
